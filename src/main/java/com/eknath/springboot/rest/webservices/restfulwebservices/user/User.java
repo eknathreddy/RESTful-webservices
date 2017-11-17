87a44e59834779044b36efe25bd641fd1b39631a
eknathreddy <eknath.eswar@gmail.com>
2017-11-17 17:56:06 -0600

    Swagger commit(API Documentation)

--- src/main/java/com/eknath/springboot/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/eknath/springboot/rest/webservices/restfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about user.")
 public class User {
 	
 	private Integer id;
 	
 	@Size(min=2, message="Name must be atleast 2 characters")
+	@ApiModelProperty(notes="should be 2 characters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Dob should be in the past")
 	private Date dob;
 	
 	public User() {
