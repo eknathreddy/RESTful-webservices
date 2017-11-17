9d7ee06f402b2ed19733122301ea98cf05983656
eknathreddy <eknath.eswar@gmail.com>
2017-11-17 00:59:47 -0600

    First commit

---
+++ src/main/java/com/eknath/springboot/rest/webservices/restfulwebservices/RestfulWebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.eknath.springboot.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eknath/springboot/rest/webservices/restfulwebservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eknath.springboot.rest.webservices.restfulwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
